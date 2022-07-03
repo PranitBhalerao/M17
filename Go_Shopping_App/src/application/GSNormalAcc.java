8eb3b1e058cff1764d784a1e3cdd517d146a3901
A computer <A computer@Acomputer-PC>
2022-07-03 15:18:24 +0530

    case_Study

---
+++ Go_Shopping_App/src/application/GSNormalAcc.java
@@ -0,0 +1,22 @@
+package application;
+import framework.NormalAcc;
+public class GSNormalAcc extends NormalAcc {
+//constructor
+	public GSNormalAcc(int accNo, String accNm, float charges, float deliveryCharges) {
+		super(accNo, accNm, charges, deliveryCharges);
+	}
+	
+	
+	
+	@Override
+	public void bookProduct(float charges) {
+//		
+		System.out.println(  "Normal user your charges are "+ getCharges() + " "+"with Delivery charges is  "+ getDeliverycharges());
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+}
